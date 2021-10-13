a31c5a5d7903cdaef0d500bca0ceb8813951acee
Tsveti1103 <cvstoqnova@gmail.com>
2021-10-13 16:06:01 +0300

    lab

---
+++ Python_basics/conditional_statements_advanced_lab/Animal Type.py
@@ -0,0 +1,7 @@
+name = input()
+if name == "dog":
+    print("mammal")
+elif name == "crocodile" or name == "tortoise" or name == "snake":
+    print("reptile")
+else:
+    print("unknown")
---
+++ Python_basics/conditional_statements_advanced_lab/Cinema Ticket.py
@@ -0,0 +1,7 @@
+day_of_week = input()
+if day_of_week == "Monday" or day_of_week == "Tuesday" or day_of_week == "Friday":
+    print("12")
+elif day_of_week == "Wednesday" or day_of_week == "Thursday":
+    print("14")
+elif day_of_week == "Saturday" or day_of_week == "Sunday":
+    print("16")
