034662b479e56d1a62c8535308212baec5e4e051
Tsveti1103 <cvstoqnova@gmail.com>
2021-11-04 16:18:55 +0200

    Create Greeting_by_name_v2.py

---
+++ Python_basics/first_steps_in_coding_lab/Greeting_by_name_v2.py
@@ -0,0 +1,7 @@
+first_name = input()
+last_name = input()
+age = int(input())
+town = input()
+
+print('You are {0} {1}, a {2}-years old person from {3}.'
+      .format(first_name, last_name, age, town))
