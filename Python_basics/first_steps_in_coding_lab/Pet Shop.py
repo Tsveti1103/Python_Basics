702c3e9603253537558319406b50a63650386e4e
Tsveti1103 <cvstoqnova@gmail.com>
2021-11-04 16:19:04 +0200

    Create Pet Shop.py

---
+++ Python_basics/first_steps_in_coding_lab/Pet Shop.py
@@ -0,0 +1,6 @@
+number_of_dogs = int(input())
+number_of_other_animals = int(input())
+dog_food_price = number_of_dogs*2.5
+other_animals_food_price = number_of_other_animals*4
+needed_money = dog_food_price + other_animals_food_price
+print(f"{needed_money} lv.")
