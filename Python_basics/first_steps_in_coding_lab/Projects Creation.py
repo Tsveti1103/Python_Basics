5dbbc8eb895bb879dbe5cfd562473ebba1812667
Tsveti1103 <cvstoqnova@gmail.com>
2021-11-05 16:19:11 +0200

    Create Projects Creation.py

---
+++ Python_basics/first_steps_in_coding_lab/Projects Creation.py
@@ -0,0 +1,4 @@
+architect_name = input()
+number_of_projects = int(input())
+needed_time =number_of_projects * 3
+print(f"The architect {architect_name} will need {needed_time} hours to complete {number_of_projects} project/s.")
