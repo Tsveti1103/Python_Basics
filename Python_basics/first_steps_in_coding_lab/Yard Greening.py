0927cf42ed0f204f038a5678b4efbfc494201c8b
Tsveti1103 <cvstoqnova@gmail.com>
2021-11-05 16:19:15 +0200

    Create Yard Greening.py

---
+++ Python_basics/first_steps_in_coding_lab/Yard Greening.py
@@ -0,0 +1,6 @@
+area = float(input())
+price = area*7.61
+discount = price*0.18
+final_price = price-discount
+print(f"The final price is: {final_price} lv.")
+print(f"The discount is: {discount} lv.")
