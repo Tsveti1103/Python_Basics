90571a41ef6d86bb37bde5eecd9469224c606ff5
Tsveti1103 <cvstoqnova@gmail.com>
2021-11-25 16:25:31 +0200

    Create combinations.py

---
+++ Python_basics/nested_loops_lab/combinations.py
@@ -0,0 +1,9 @@
+n = int(input())
+counter = 0
+for x1 in range(0, n + 1):
+    for x2 in range(0, n + 1):
+        for x3 in range(0, n + 1):
+            result = x1 + x2 + x3
+            if result == n:
+                counter += 1
+print(counter)
