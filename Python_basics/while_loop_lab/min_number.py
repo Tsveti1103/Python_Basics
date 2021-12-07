87ff747a42b239dc658c3066ec040b4726f14b2a
Tsveti1103 <cvstoqnova@gmail.com>
2021-12-07 16:28:18 +0200

    Create min_number.py

---
+++ Python_basics/while_loop_lab/min_number.py
@@ -0,0 +1,10 @@
+import sys
+
+number = input()
+min_number = sys.maxsize
+while number != "Stop":
+    number = int(number)
+    if min_number > number:
+        min_number = number
+    number = input()
+print(min_number)
